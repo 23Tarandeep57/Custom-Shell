de3e35a88bf77c19e49e031c911a5d215d2ff7d0
Tarandeep Singh <deeptaran2004bti@gmail.com>
2025-10-31 13:29:48 +0530

    test

--- src/main.cpp
+++ src/main.cpp
@@ -139,8 +139,7 @@ int main() {
         cout << "$taran> ";
         cout << "taklu$ ";
         cout << "testing this git thingy";
-        cout << "testing this git thingy again";
-        cout << "testing this git thingy";
+
         string input;
         getline(cin, input);
 
